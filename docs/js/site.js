d154371ad2df60d6d7b7ee485cfcf71287ac0c63
Caitlin Sanders <csanders1999@hotmail.com>
2025-01-23 22:59:03 -0500

    MAPgit statusgit status

--- docs/js/site.js
+++ docs/js/site.js
@@ -2,3 +2,10 @@ function navScroll(section) {
   var elem = document.getElementById(section);
   window.scroll(0, elem.offsetTop - 40);
 }
+
+var map = L.map('map').setView([40.76, -73.98], 12);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
